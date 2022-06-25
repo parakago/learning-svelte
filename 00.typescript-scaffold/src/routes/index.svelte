1902aaf3a28c7fbf027e56fef924618805860402
parakago <parakago@gmail.com>
2022-06-25 23:24:48 +0900

    test

--- 00.typescript-scaffold/src/routes/index.svelte
+++ 00.typescript-scaffold/src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+export const helo: string = 'Very Welcome';
+</script>
+
+<h1>{helo} to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
